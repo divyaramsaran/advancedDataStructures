5474e022e83acf3fbca6e01c9a162cedaf605bb0
Ram Saran <183719077+divyaramsaran@users.noreply.github.com>
2025-08-07 11:13:12 +0000

    takes input for each node

--- binarySearchTree.c
+++ binarySearchTree.c
@@ -2,13 +2,26 @@
 #include<stdlib.h>
 struct node {
     int data;
-    struct node * left;
-    struct node * right;
+    struct node * left, *right;
 };
 
-struct node *newnode, *root;
+srtuct node * insert (struct node * root, value) {
+    return root;
+}
+
 
 int main() {
+    struct node *newnode, *root;
+    int data, input;
     root = 0;
+    printf("Enter how many elements you want to add");
+    scanf("%d", &input);
+
+    for (int range = 0; range < input; range++) {
+        printf("Enter data");
+        scanf("d", &data);
+        root = insert(root, data);
+    }
+
     newnode = (struct node *) malloc(sizeof(struct node));
 }
