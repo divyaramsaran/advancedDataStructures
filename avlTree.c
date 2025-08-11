6c42a3bfb431f02c9a03e9e7c681e211ce75be56
Ram Saran <183719077+divyaramsaran@users.noreply.github.com>
2025-08-11 12:34:49 +0000

    adds create function and balancing height

--- avlTree.c
+++ avlTree.c
@@ -7,6 +7,32 @@ struct node {
     int height;
 };
 
+int height(struct node *node) {
+    if(node == NULL) {
+        return 0;
+    }
+    return node->height;
+}
+
+struct node *create (struct node *root, int value) {
+    struct node *newNode;
+    if(root == NULL) {
+        newNode = (struct node *) malloc (sizeof(struct node));
+        root = newNode;
+        root -> left = NULL;
+        root -> right = NULL; 
+        root -> data = value;
+        root -> height = 1;
+    }
+    else if(value < root -> data) {
+        root -> left = create (root -> left, value);
+    }
+    else if(value > root -> data) {
+        root -> right = create (root -> right, value);
+    }
+    return root;
+}
+
 int main() {
     struct node *root = NULL;
     int elements;
