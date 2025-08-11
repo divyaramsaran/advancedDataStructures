abd57ee6b8c80439a98550267368f275be2affad
Ram Saran <183719077+divyaramsaran@users.noreply.github.com>
2025-08-11 12:32:43 +0000

    adds basic template of tree

--- avlTree.c
+++ avlTree.c
@@ -1,2 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+struct node {
+    int data;
+    struct node *left;
+    struct node *right;
+    int height;
+};
+
+int main() {
+    struct node *root = NULL;
+    int elements;
+    printf("Enter how many elements you want to add");
+    scanf("%d", &elements);
+
+    printf("Enter node value: ");
+    for(int i = 0; i < elements; i++) {
+        int value;
+        scanf("%d", &value);
+        root = create(root, value);
+    }
+
+    return 0;
+}
